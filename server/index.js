e67430cc782128bc57f2856d139e99b8b9417b33
Gero <geronimolazzarino@gmail.com>
2020-01-24 14:31:34 -0300

    fixes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-'use strict'
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const { mongoose } = require('./database');
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.json());
-app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// app.get('/*', function (req, res) {
+//     res.sendFile('index.html', { root: path.join(__dirname, 'dist') });
+// });
 
 app.use('/api', require('./routes/task.routes'));
 
